7a1342277de502bb5d704bc107c6c0de183ac92a
Joyce Yeung <joyce.yeung@myob.com>
2020-09-16 13:49:18 +1000

    Kata completed

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public StringCalculator()
+        {
+        }
+
+        private List<string> _delimiters = new List<string> { ",", "\n" };
+
+        public int Calculate(string input)
+        {
+
+            if (HasCustomDelimiter(input, out Match matchResult))
+            {
+                AddDelimiter(matchResult);
+            }
+
+            string[] inputs = input.Split(_delimiters.ToArray(), StringSplitOptions.None);
+
+            int sumResult = 0;
+            List<int> negativeNumbers = new List<int>();
+            foreach (var splitstring in inputs)
+            {
+                if (Int32.TryParse(splitstring, out int number))
+                {
+                    if (number < 0)
+                    {
+                        negativeNumbers.Add(number);
+                    }
+                    else if (number < 1000)
+                    {
+                        sumResult += number;
+                    }
+                }
+
+            }
+            if (negativeNumbers.Count > 0)
+            {
+                throw new NegativeNumberException($"Negatives not allowed: {string.Join(", ", negativeNumbers)}");
+            }
+
+            return sumResult;
+
+
+        }
+
+        private bool HasCustomDelimiter(string input, out Match m)
+        {
+            string pattern = @"\/\/(.+?)\n";
+            Regex regex = new Regex(pattern);
+            m = regex.Match(input);
+            return m.Success;
+
+        }
+
+        private void AddDelimiter(Match matchResult)
+        {
+            string delimiterField = matchResult.Groups[1].Value;
+            if (HasMultipleDelimilters(delimiterField, out MatchCollection m))
+            {
+                foreach (Match match in m)
+                {
+                    _delimiters.Add(match.Groups[1].Value);
+                }
+            }
+            else
+            {
+                _delimiters.Add(delimiterField);
+            }
+        }
+
+        private bool HasMultipleDelimilters(string delimiterField, out MatchCollection m)
+        {
+            string pattern = @"\[(.+?)\]";
+            Regex regex = new Regex(pattern);
+            m = regex.Matches(delimiterField);
+            return m.Count > 0;
+
+        }
+    }
+}
---
+++ StringCalculator_test/StringCalculatorTests.cs
@@ -0,0 +1,131 @@
+using System;
+using Xunit;
+
+namespace StringCalculator
+{
+    public class StringCalculatorTests
+    {
+        StringCalculator calculator;
+
+        public StringCalculatorTests()
+        {
+            calculator = new StringCalculator();
+        }
+
+        
+        [Fact]
+        public void GivenEmptyString_ShouldReturn0()
+        {
+            var actual = calculator.Calculate("");
+            var expected = 0;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("3", 3)]
+
+        public void GivenNumber_ShouldReturnNumber(string input, int value)
+        {
+            var actual = calculator.Calculate(input);
+
+            Assert.Equal(value, actual);
+
+        }
+
+        [Fact]
+        public void GivenTwoNumbers_ShouldReturnSum()
+        {
+            var actual = calculator.Calculate("3,5");
+            var expected = 8;
+
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void GivenAnyNumbers_ShouldReturnSum()
+        {
+            var actual = calculator.Calculate("3,5,3,9");
+            var expected = 20;
+
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void GivenThereIsNewLineBreaksBetweenNumbers_ShouldReturnSum()
+        {
+            var actual = calculator.Calculate("3\n5\n3,9");
+            var expected = 20;
+
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void GivenDifferentDelimiters_ShouldReturnSum()
+        {
+            var actual = calculator.Calculate("//;\n1;2");
+            var expected = 3;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenNegativeNumbers_ShouldThrowNegativeException()
+        {
+            Action act = () => calculator.Calculate("-1,2,-3");
+            //assert
+            NegativeNumberException exception = Assert.Throws<NegativeNumberException>(act);
+            //The thrown exception can be used for even more detailed assertions.
+            Assert.Equal("Negatives not allowed: -1, -3", exception.Message);
+        }
+
+        [Fact]
+        public void GivenNumberLargerThan1000_ShouldBeIgnored()
+        {
+            var actual = calculator.Calculate("1000,1001,2");
+            var expected = 2;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenDelimitersWithAnyLength_ShouldReturnSum()
+        {
+            var actual = calculator.Calculate("//[***]\n1***2***3");
+            var expected = 6;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenMultipleDelimiters_ShouldReturnSum()
+        {
+            var actual = calculator.Calculate("//[*][%]\n1*2%3");
+            var expected = 6;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenMultipleDelimitersLongerThanOneChar_ShouldReturnSum()
+        {
+            var actual = calculator.Calculate("//[***][#][%]\n1***2#3%4");
+            var expected = 10;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenDelimiterWithNumberInBetween_ShouldReturnSum()
+        {
+            var actual = calculator.Calculate("//[*1*][%]\n1*1*2%3");
+            var expected = 6;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
